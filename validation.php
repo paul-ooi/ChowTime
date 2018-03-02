5cf8ebe547f53537058ed31f2861f8593c09d0d1
Sophia <sophiavong@live.ca>
2018-03-02 15:41:21 -0500

    validation

---
+++ validation.php
@@ -0,0 +1,16 @@
+<?php
+class Validation {
+    // Email Validation - Sophia Vong
+    public function email($emailParam){
+        $emailRegex = '/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/';
+        $emailMatch = preg_match($emailRegex, $emailParam);
+        return $emailMatch; // Returns 1 or 0 for true or false
+    }
+
+    // Numbers Only Validation - Sophia Vong
+    public function number($numbParam){
+        $numbRegex = '/^\d*$/';
+        $numbMatch = preg_match($numbRegex, $numbParam);
+        return $numbMatch; // Returns 1 or 0 for true or false
+    }
+}
