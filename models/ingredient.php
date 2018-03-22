58f3fcf1c95ef64a708f821f1c5f2dae9d189819
Paul O <info@paulooidesign.com>
2018-03-22 09:46:34 -0400

    add ingredient object (WIP) and ingredient Form

--- models/ingredient.php
+++ models/ingredient.php
@@ -1,25 +1,26 @@
 <?php
 
 class Ingredient {
-    private $recipeRef;//Reference ID for the recipe
-    private $name;
+    private $id;
+    private $recipe_id;//Reference ID from the Recipe table
+    private $food_id;//Reference ID from the Food Table
     private $quantity;
-    private $measurment;
+    private $unit; //Tablespoon, Cup, ML
     private $preparation;
-    private $optional;
+    private $required;
 
     public function getRecipeRef() {
-        return $this->recipeRef;
+        return $this->recipe_id;
     }
     public function setRecipeRef($param) {
-        $this->recipeRef = $param;
+        $this->recipe_id = $param;
     }
 
-    public function getName() {
-        return $this->name;
+    public function getFoodId() {
+        return $this->food_id;
     }
-    public function setName($param) {
-        $this->name = $param;
+    public function setFoodId($param) {
+        $this->food_id = $param;
     }
 
     public function getQuantity() {
@@ -29,11 +30,11 @@ class Ingredient {
         $this->quantity = $param;
     }
 
-    public function getMeasurment() {
-        return $this->measurment;
+    public function getUnit() {
+        return $this->unit;
     }
-    public function setMeasurment($param) {
-        $this->measurment = $param;
+    public function setUnit($param) {
+        $this->unit = $param;
     }
 
     public function getPreparation() {
@@ -44,28 +45,44 @@ class Ingredient {
     }
 
     public function getOptional() {
-        return $this->optional;
+        return $this->required;
     }
     public function setOptional($param) {
-        $this->optional = $param;
+        $this->required = $param;
     }
 
-    public function __construct($name, $qty = null, $measure = null, $prep = null, $optional = false) {
-        $this->setName($name);
-        $this->setQuantity($qty);
-        $this->setMeasurment($measure);
-        $this->setPreparation($prep);
-        $this->setOptional($optional);
-    }
+    // public function __construct($r_id, $qty = null, $unit = null, $prep = null, $required = true) {
+    //     $this->setRecipeRef($r_id);
+    //     $this->setQuantity($qty);
+    //     $this->setMeasurment($unit);
+    //     $this->setPreparation($prep);
+    //     $this->setOptional($required);
+    // }
 
-    public static function displayAllIngredients($db) {
-        $sql = "SELECT * FROM ingredients";
-        $pdostm = $db->prepare($sql);
+    public function displayAllIngredients($db) {
+        $query = "SELECT * FROM ingredients";
+        $pdostm = $db->prepare($query);
 
         $pdostm->setFetchMode(PDO::FETCH_OBJ);
         $pdostm->execute();
 
         return $pdostm->fetchAll();
+    }
+
+    public function getRecipeTitles($db) {
+        $query = "SELECT id, title FROM recipes";
+        $pdostm = $db->prepare($query);
+
+        $pdostm->execute();
+        return $pdostm->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function getFoodNames($db) {
+        $query = "SELECT id, food_name FROM foods";
+        $pdostm = $db->prepare($query);
+
+        $pdostm->execute();
+        return $pdostm->fetchAll(PDO::FETCH_OBJ);
     }
 
 }
