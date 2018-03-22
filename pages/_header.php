e145b368a85cdbc915ce96c2893fadb897ca5fb2
Paul O <info@paulooidesign.com>
2018-03-22 09:45:10 -0400

    update header to include jQuery CDN

--- pages/_header.php
+++ pages/_header.php
@@ -4,5 +4,6 @@
         <meta charset="utf-8"/>
         <title><?= $pageTitle ?></title>
         <link href="assets/css/general.css" type="text/css" rel="stylesheet"/>
+        <script  src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="  crossorigin="anonymous"></script>
     </head>
     <body>
