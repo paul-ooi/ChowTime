5e45ac314a8ddbc94bd99c41d0ba3e3bb5a500de
Paul O <info@paulooidesign.com>
2018-03-24 08:55:53 -0400

    create main nav and contact us page WIP

---
+++ pages/_mainnav.php
@@ -0,0 +1,12 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">What's Cooking</a></li>
+            <li><a href="contactus.php">Contact Us</a></li>
+        </ul>
+    </nav>
+    <div id="logo">
+        <h2>Chow Time</h2>
+    </div>
+</header>
---
+++ pages/contactus.php
@@ -0,0 +1,16 @@
+<?php
+
+$pageTitle = 'Contact Us';
+require_once '_header.php';
+require_once '_mainnav.php';
+?>
+<main>
+<h1>Contact Us</h1>
+
+</main>
+<aside>
+    <div id="description">
+    </div>
+    <div class="office">
+    </div>
+</aside>
