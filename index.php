fbdc39f7ccc812b30276c49aee53e1bcb82c523d
Paul O <info@paulooidesign.com>
2018-03-30 23:36:32 -0400

    update global partials and index

--- index.php
+++ index.php
@@ -11,15 +11,15 @@ require_once 'pages/partial/_header.php';
     </header>
     <main class="container ddwrapper  mb-5">
         <!-- YOUR CONTENT HERE -->
-        <section class="mx-auto text-center" id="search">
+        <section class="mx-auto text-center mb-5" id="search">
             <h1 class="sr-only">Chow Time</h1>
-            <form  action="controllers/recipes/_allrecipes.php" method="post" id="searchAll" name="searchAll">
-                <h2>Find the Next recipe</h2>
+            <h2>Find your Next recipe</h2>
+            <form class="form-inline" action="controllers/recipes/_allrecipes.php" method="post" id="searchAll" name="searchAll">
                 <div class="input-group d-inline-md justify-content-center">
                     <label class="form-control-label sr-only" for="form_search">Search Recipes</label>
-                    <input class="form-control" type="text" name="form_search" id="form_search" placeholder="Start cooking..."/>
+                    <input class="form-control form-control-lg" type="text" name="form_search" id="form_search" placeholder="Start cooking..."/>
                     <span class="input-group-btn">
-                    <input class="btn" type="submit" for="searchAll" name="searchRecipesBtn" id="searchRecipesBtn" value="Search Recipes"/>
+                    <input class="btn btn-lg" type="submit" for="searchAll" name="searchRecipesBtn" id="searchRecipesBtn" value="Search Recipes"/>
                 </span>
                 </div>
             </form>
@@ -27,26 +27,30 @@ require_once 'pages/partial/_header.php';
                 <img src="assets/imgs/image1.jpg" alt="plate of spaghetti"/>
             </span> -->
         </section>
+        <section class="mx-auto mb-3" id="searchResults">
+        </section>
         <section class="mx-auto" id="top-category">
             <h2>Top searched categories</h2>
-            <div class="gallery-item col-sm-6 col-lg-4">
+            <div class="row">
+            <div class="gallery-item col-sm-6 col-lg-4 text-center">
                 <a href="index.php"><h3>Category name</h3><img src="assets/imgs/image1.jpg" alt="plate of spaghetti" class=" img-thumbnail"/></a>
             </div>
-            <div class="gallery-item col-sm-6 col-lg-4">
+            <div class="gallery-item col-sm-6 col-lg-4 text-center">
                 <a href="index.php"><h3>Category name</h3><img src="assets/imgs/image1.jpg" alt="plate of spaghetti" class=" img-thumbnail"/></a>
             </div>
-            <div class="gallery-item col-sm-6 col-lg-4">
+            <div class="gallery-item col-sm-6 col-lg-4 text-center">
                 <a href="index.php"><h3>Category name</h3><img src="assets/imgs/image1.jpg" alt="plate of spaghetti" class=" img-thumbnail"/></a>
             </div>
-            <div class="gallery-item col-sm-6 col-lg-4">
+            <div class="gallery-item col-sm-6 col-lg-4 text-center">
                 <a href="index.php"><h3>Category name</h3><img src="assets/imgs/image1.jpg" alt="plate of spaghetti" class=" img-thumbnail"/></a>
             </div>
-            <div class="gallery-item col-sm-6 col-lg-4">
+            <div class="gallery-item col-sm-6 col-lg-4 text-center">
                 <a href="index.php"><h3>Category name</h3><img src="assets/imgs/image1.jpg" alt="plate of spaghetti" class=" img-thumbnail"/></a>
             </div>
-            <div class="gallery-item col-sm-6 col-lg-4">
+            <div class="gallery-item col-sm-6 col-lg-4 text-center">
                 <a href="index.php"><h3>Category name</h3><img src="assets/imgs/image1.jpg" alt="plate of spaghetti" class=" img-thumbnail"/></a>
             </div>
+        </div>
         </section>
     </main>
     <?php require_once 'pages/partial/_footer.php' ?>
--- pages/partial/_mainnav.php
+++ pages/partial/_mainnav.php
@@ -1,10 +1,16 @@
     <nav>
-        <ul class="nav navbar-nav">
-            <li class="nav-item"><a class="nav-link" href="../events.php">Events</a></li>
-            <li class="nav-item"><a class="nav-link" href="../Whats-cooking.php">What&apos;s Cooking</a></li>
-            <li class="nav-item"><a class="nav-link" href="contactus.php">Contact Us</a></li>
+        <!-- <ul class="nav nav-pills navbar-expand-sm navbar-collapse-lg d-flex flex-row"> -->
+        <ul class="nav nav-pills navbar-collapse-lg d-flex justify-content-start">
+            <li class=""><a class="nav-link px-2" href="../events.php">Events</a></li>
+            <li class=""><a class="nav-link px-2" href="../Whats-cooking.php">What&apos;s Cooking</a></li>
+            <li class=""><a class="nav-link px-2" href="controllers/contactus.php">Contact Us</a></li>
+            <li class=""><a class="nav-link px-2 " href="controllers/login.php">Login</a></li>
+            <!-- <li class="nav-item"><a class="nav-link px-2" href="../events.php">Events</a></li>
+            <li class="nav-item"><a class="nav-link px-2" href="../Whats-cooking.php">What&apos;s Cooking</a></li>
+            <li class="nav-item"><a class="nav-link px-2" href="controllers/contactus.php">Contact Us</a></li>
+            <li class="nav-item"><a class="nav-link px-2 " href="controllers/login.php">Login</a></li> -->
         </ul>
     </nav>
-    <div class="row mx-auto" id="title">
-        <h2><a href="index.php">Chow <img class="mx-auto" id="logo" src="../assets/imgs/ct-logo.png" alt="Chow Time logo - Plate with fork and knife arranged like locator marker"/> Time</a></h2>
+    <div class="text-center mx-auto" id="title">
+        <h2><a href="index.php">Chow <img class="mx-auto" id="logo" src="assets/imgs/ct-logo.png" alt="Chow Time logo - Plate with fork and knife arranged like locator marker"/> Time</a></h2>
     </div>
