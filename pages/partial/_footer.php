29e5f84ebaae56b78662d12f9a06141c0ec4b70f
Paul O <info@paulooidesign.com>
2018-03-31 00:22:53 -0400

    fixed footer links

--- pages/partial/_footer.php
+++ pages/partial/_footer.php
@@ -1,14 +1,14 @@
 <footer class="container ddwrapper">
     <!-- SITE MAP LINKS -->
-    <nav class="col-sm-12 col-lg-8 mb-4">
-        <ul id="sitemap">
-            <li><a href="pages/events.php" title="What events are happening">Events</a></li>
-            <li><a href="pages/events.php" title="What events are happening">What&apos;s Cooking</a></li>
-            <li><a href="contactus.php">Contact Us</a></li>
-            <li><a href="register.php">Create Account</a></li>
+    <nav class="col-sm-12 col-lg-6 mb-4">
+        <ul class="nav d-flex-sm" id=" sitemap ">
+            <li><a class="nav-link" href="pages/events.php" title="What events are happening">Events</a></li>
+            <li><a class="nav-link" href="pages/events.php" title="What events are happening">What&apos;s Cooking</a></li>
+            <li><a class="nav-link" href="contactus.php">Contact Us</a></li>
+            <li><a class="nav-link" href="register.php">Create Account</a></li>
         </ul>
     </nav>
-    <section class="col-sm-3 col-lg-4 d-flex justify-content-lg-around justify-content-sm-between" id="social-media">
+    <section class="col-sm-3 col-lg-6 d-flex justify-content-lg-around justify-content-sm-between" id="social-media">
         <h2 class="sr-only">Follow us on Social Media</h2>
         <div class="social" id="fbook">
             <a href="http://www.facebook.com"><img src="assets/icons/social-fb.svg" alt="Like and Follow us on Facebook" title="Like and Follow us on Facebook"/></a>
