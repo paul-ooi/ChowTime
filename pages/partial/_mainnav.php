9296947d3770d4a8c2c6e8f2639663a4e61c22a7
Jessica Wong <jessica.pearl.wong@gmail.com>
2018-04-01 20:18:30 -0400

    fixed mainnav and footer file path links and added one to recipe.

--- pages/partial/_mainnav.php
+++ pages/partial/_mainnav.php
@@ -1,11 +1,11 @@
     <nav id="main-nav">
         <ul class="nav nav-pills">
-            <li><a class="nav-link px-2" href="../events.php">Events</a></li>
-            <li><a class="nav-link px-2" href="pages/whatsCooking.php">What&apos;s Cooking</a></li>
-            <li><a class="nav-link px-2" href="controllers/contactus.php">Contact Us</a></li>
-            <li><a class="nav-link px-2 " href="controllers/login.php">Login</a></li>
+            <li><a class="nav-link px-2" href="/chowtime/pages/events.php">Events</a></li>
+            <li><a class="nav-link px-2" href="/chowtime/pages/whatsCooking.php">What&apos;s Cooking</a></li>
+            <li><a class="nav-link px-2" href="/chowtime/pages/contactus.php">Contact Us</a></li>
+            <li><a class="nav-link px-2 " href="/chowtime/pages/controllers/login.php">Login</a></li>
         </ul>
     </nav>
     <div class="text-center mx-auto" id="title">
-        <h2><a href="index.php">Chow <img class="mx-auto" id="logo" src="../assets/imgs/ct-logo.png" alt="Chow Time logo - Plate with fork and knife arranged like locator marker"/> Time</a></h2>
+        <h2><a href="/chowtime/index.php">Chow <img class="mx-auto" id="logo" src="/chowtime/assets/imgs/ct-logo.png" alt="Chow Time logo - Plate with fork and knife arranged like locator marker"/> Time</a></h2>
     </div>
