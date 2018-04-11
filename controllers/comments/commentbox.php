dd4e25390cfe2a79efff4aa2ada91151165a954a
Sophia <sophiavong@live.ca>
2018-04-10 23:06:00 -0400

    comments

--- controllers/comments/commentbox.php
+++ controllers/comments/commentbox.php
@@ -1,17 +1,33 @@
 <?php include '../../pages/partial/_header.php'; ?>
 </head>
 
+<?php
+    require_once '../../models/db.php';
+    require_once '../../models/comment.php';
+
+    if (isset($_POST['add'])){
+        $recipeId = $_POST['recipeIdInput'];
+        $userId = $_POST['userIdInput'];
+        $comment = $_POST['commentInput'];
+        $db = Database::getDb();
+        $c = new Comment();
+        $count = $c->addComment($db, $recipeId, $userId, $comment);
+        if (!$count) {
+            echo "Problem Adding.";
+        }
+    }
+?>
+
 <section id="commentBox">
-        <h2>Add Comment</h2>
+        <h3>Add Comment</h3>
         <form action="" method="post" id="addComment">
             <div>
                 <textarea class="form-control" rows="4"></textarea>
             </div>
             <div>
-                <form action="" method="post" name="addComment">
-                    <input type="hidden" value="<?php // Event Id ?>" />
-                    <input type="submit" class="btn btn-default" id="comSub" value="Submit" />
-                </form>
+                <input type="hidden" value="<?php // Event Id ?>" />
+                <input type="hidden" value="<?php // User Id ?>" />
+                <input type="submit" class="btn btn-default" name="add" value="Post" />
             </div>
         </form>
 </section>
