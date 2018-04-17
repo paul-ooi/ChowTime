92722df3a1787ca4ef296d4d1c54092d1614d669
Paul O <info@paulooidesign.com>
2018-04-17 14:38:06 -0400

    Added functionality of i-made-it button in search results

--- controllers/search/index.php
+++ controllers/search/index.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+// $_SESSION['user_id'] = 68;
 //if someone accesses this page with out actually searching for something it will redirect to the main home page
 // if (!isset($_GET['action'])) {
 //     header("Location: ./index.php");
@@ -46,7 +48,14 @@ switch($_GET['action']) {
         <div class="col-lg-4 d-flex flex-wrap justify-content-start px-0   ">
             <a href="<?php echo 'pages/recipes.php?&id='. $r->recipe_id ?>" class="btn btn-default col-md-5 mx-2">View Recipe</a>
             <!-- <input type="button" name="viewRecipe" class="btn btn-default col-md-5 mx-2" value="View Recipe" /> -->
-            <input type="button" name="madeIt" class="btn btn-default col-md-5 mx-2" value="I Made it" />
+            <!-- Button for I Made it is only for ractive users -->
+            <?php if (isset($_SESSION['user_id'])) { ?>
+                <form action="controllers/Recipe/_i-made-it.php" method="post" name="iMadeIt" class="col-md-6">
+                    <input type="hidden" name="recipe_id" value="<?php echo $r->recipe_id ?>"/>
+                    <input type="hidden" name="user_id" value="<?php echo $_SESSION['user_id'] ?>"/>   
+                    <input type="submit" name="madeIt" class="btn btn-default col-12" value="I Made it" />
+                </form>
+            <?php }?>
         </div>
 
     </li>
