4d3fbf8152facc53059b619d949b729490c4ce93
Sophia <sophiavong@live.ca>
2018-04-23 19:52:28 -0400

    recipe->comment

--- controllers/comments/commentbox.php
+++ controllers/comments/commentbox.php
@@ -9,7 +9,7 @@ $l = new LikesDislikes;
 // COMMUNITY RATING SYSTEM
 $like_class = "";
 $dislike_class = "";
-// Checking # of likes/dislikes
+
 $likes = $l->getLikesByRecipe($db, $recipe_id);
 $dislikes = $l->getDislikesByRecipe($db, $recipe_id);
 
@@ -20,13 +20,11 @@ $input_type = 'hidden';
 $upd = 'update';
 $edit = '<i class="fas fa-edit"></i>';
 
-
-// Adding a comment
 if (isset($_POST['add'])){
-    $recipeId = $_POST['recipeIdInput'];
+    $recipeId = $recipe_id;
     $userId = $_SESSION['user_id'];
     $comment = $_POST['commentInput'];
-    $count = $c->addComment($db, $recipeId, $userId, $comment);
+    $count = $c->addRecipeComment($db, $recipeId, $userId, $comment);
     if (!$count) {
         echo "Problem Adding.";
     }
@@ -35,10 +33,10 @@ if (isset($_POST['add'])){
 // Update Comments
 if (isset($_POST['update'])){
     // Hide <p> tag and reveal textarea input
-    $class = 'hidden';
+    $class = 'd-none';
     $input_type = 'textarea';
     $upd = 'upd'; // Alter the button for updating the comment
-
+    $edit = 'Save Changes';
     $id = $_POST['id'];
     $com = $c->getCommentById($db, $id);
 }
@@ -52,7 +50,6 @@ if (isset($_POST['upd'])){
     $id = $_POST['id'];
     $comment = $_POST['comment'];
     $count = $c->updateComment($db, $id, $comment);
-
     // If there's a problem with the update, display error message
     if (!$count){
         echo "Problem Updating.";
@@ -69,9 +66,7 @@ if (isset($_POST['delete'])){
     }
 }
 
-// Only Show if user is logged in
-if (isset($_SESSION['user_id'])) {
-
+if (isset($_SESSION['user_id'])){
     // Checking for Likes/Dislikes
     $check_e_like = $l->checkUserLikeRecipe($db, $recipe_id, $_SESSION['user_id']);
     $check_e_dislike = $l->checkUserDisLikeRecipe($db, $recipe_id, $_SESSION['user_id']);
@@ -100,7 +95,7 @@ if (isset($_SESSION['user_id'])) {
         $check_e_dislike = $l->checkUserDisLikeRecipe($db, $recipe_id, $_SESSION['user_id']);
 
         if ($check_e_dislike != null) {
-            $l->deleteDisikeRecipe($db, $recipe_id, $_SESSION['user_id']);
+            $l->deleteDisikRecipe($db, $recipe_id, $_SESSION['user_id']);
         } else if ($check_e_dislike == null && $check_e_like == null) {
             $l->addDislikeRecipe($db, $recipe_id, $_SESSION['user_id']);
         } else if ($check_e_dislike == null && $check_e_like != null) {
@@ -112,62 +107,76 @@ if (isset($_SESSION['user_id'])) {
         $dislikes = $l->getDislikesByRecipe($db, $recipe_id);
     }
 ?>
-
-<section id="commentBox">
-        <h2>Add Comment</h2>
+<section id="communityRate">
+    <div class="wrapper">
         <form action="" method="post">
+            <div class="btn-group btn-group-lg" role="group" aria-label="...">
+                <!-- Like -->
+                <button class="btn btn-default" type="submit" name="like"><i class="fas fa-thumbs-up"></i> <?php echo $likes[0]->count; ?></button>
+                <!-- Dislike -->
+                <button class="btn btn-default" type="submit" name="dislike"><i class="fas fa-thumbs-down"></i> <?php echo $dislikes[0]->count; ?></button>
+            </div>
+        </form>
+    </div>
+</section>
+<section id="commentBox">
+    <div class="wrapper">
+        <h3>Add Comment</h3>
+        <form action="details.php" method="post">
             <div>
                 <textarea class="form-control" name="commentInput" rows="4"></textarea>
             </div>
             <div>
-                <input type="hidden" name="recipeIdInput" value="<?php echo $recipe_id; ?>" />
                 <input type="submit" class="btn btn-default" name="add" value="Post" />
             </div>
         </form>
+    </div>
 </section>
-<?php }
-
- echo '<h3>Comments</h3>';
-    $comments = $c->getRecipeComments($db, $recipe_id);
-    if ($comments == null) {
-        echo '<p> There are no comments yet. </p>';
-    }
-    foreach ($comments as $cm) {
-        $user = $p->getProfileById($db, $cm->user_id);
-
-        echo '<div class="media">';
-        echo '<div class="media-left">';
-            echo '<form action="../../pages/yourProfile.php">';
-                echo '<img class="media-object rounded-circle" src="" alt="" >'; // src="to be added" alt ="' . $user->fname . ' ' . $user->lname . 'profile picture"
-            echo '</form>';
+<section>
+    <div class="wrapper">
+<?php
+}
+$comments = $c->getRecipeComments($db, $recipe_id);
+echo '<h3>Comments</h3>';
+if ($comments == null){
+    echo '<p> There are no comments yet. </p>';
+}
+foreach ($comments as $cm) {
+    $user = $p->getProfileById($db, $cm->user_id);
+    echo '<div class="media">';
+    echo '<a href="userProfile.php?id=' . $user->id . '">';
+        echo '<div class="media-left rounded-circle" style="height: 75px; width: 75px;background-image: url(../' . $user->pimage .'); background-size: cover; background-position:center;">';
         echo '</div>'; // End of media-left
+    echo '</a>';
 
-        echo  '<div class="media-body">';
+    echo '<div class="media-body">';
 
-        echo '<button class="comProfile" name="user_id" value="'. $cm->user_id .'"><span><u>' . $user->fname . ' ' . $user->lname . '</u></span></button>';
-        // For users who are logged in to edit and delete these comments
-        if (isset($_SESSION['user_id'])){
-             if ($_SESSION['user_id'] == $cm->user_id){
-                echo '<form action="" method="post">
-                <div class="row">
-                <div class="col">
-                    <span class="' . $class . '">' . $cm->comment . '</span>
-                    <input type="hidden" name="id" value="' . $cm->id . '" />
-                    <input type="' . $input_type . '" class="form-control" name="comment" value="' . $cm->comment . '" rows="3"/>
-                </div>
-                <div class="col-md-auto col-sm-auto">
-                    <button type="submit" class="btn btn-outline-info" name="' . $upd . '">' . $edit . '</button>
-                    <button type="submit" class="btn btn-outline-danger" name="delete"><i class="fas fa-trash-alt"></i></button>
-                </div>
-                </div>
-                </form>';
-             } // End of Current User Check
-             else {
-                 echo '<div><span>' . $cm->comment . '</span></div>';
-             }
-         } else {
-            echo '<div><span>' . $cm->comment . '</span></div>';
+    echo '<a href="userProfile.php?id='. $cm->user_id .'"><u>' . $user->fname . ' ' . $user->lname . '</u></a>';
+    // For users who are logged in to edit and delete these comments
+    if (isset($_SESSION['user_id'])){
+         if ($_SESSION['user_id'] == $cm->user_id){
+            echo '<form action="" method="post">
+            <div class="row">
+            <div class="col">
+                <span class="' . $class . '">' . $cm->comment . '</span>
+                <input type="hidden" name="id" value="' . $cm->id . '" />
+                <input type="' . $input_type . '" class="form-control" name="comment" value="' . $cm->comment . '" rows="3"/>
+            </div>
+            <div class="col-md-auto col-sm-auto">
+                <button type="submit" class="btn btn-outline-info" name="' . $upd . '">' . $edit . '</button>
+                <button type="submit" class="btn btn-outline-danger" name="delete"><i class="fas fa-trash-alt"></i></button>
+            </div>
+            </div>
+            </form>';
+         } // End of Current User Check
+         else {
+             echo '<div><span>' . $cm->comment . '</span></div>';
          }
-        echo '<div><span><i>Posted on ' . $cm->date . '</i></span></div></div></div>';
-        }
+     } else {
+        echo '<div><span>' . $cm->comment . '</span></div>';
+     }
+    echo '<div><span><i>Posted on ' . $cm->date . '</i></span></div></div></div>';
+}
 ?>
+</div>
+</section>
