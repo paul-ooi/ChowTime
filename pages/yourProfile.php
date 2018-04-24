03bd499477cb2c5bb753652b35e02b7e7f952f1c
Brad Campbell <campbellb92@hotmail.com>
2018-04-24 09:11:57 -0400

    loop gone?

--- pages/yourProfile.php
+++ pages/yourProfile.php
@@ -55,12 +55,16 @@ require_once 'partial/_mainnav.php';
 			$count = 0;
 			foreach($yourRecipes as $recipe)
 			{
-				$totalRecipe = $p->recipeById($db, $recipe->id);
-				?>
-				<div class="gallery-item col-sm-6 col-lg-3 text-center">
-					<a href="recipes.php?&id=<?php echo $recipe->id ?>"><h3><?php echo $totalRecipe->title; ?></h3><img src="<?php echo $totalRecipe->img_src; ?>" alt="Image of <?php echo $totalRecipe->title; ?>" class="img-fluid rounded"/></a>
-				</div>
+                if($count >= $diff)
+				{
+                    $totalRecipe = $p->recipeById($db, $recipe->id);
+                    ?>
+                    <div class="gallery-item col-sm-6 col-lg-3 text-center">
+                        <a href="recipes.php?&id=<?php echo $recipe->id ?>"><h3><?php echo $totalRecipe->title; ?></h3><img src="<?php echo $totalRecipe->img_src; ?>" alt="Image of <?php echo $totalRecipe->title; ?>" class="img-fluid rounded"/></a>
+                    </div> //here
 			<?php	
+                }
+                $count++;
 			}
 			?>
 			</div>
